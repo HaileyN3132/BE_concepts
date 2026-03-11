82384bde659f27d76c7cca9883d9920923924541
Hailey <ngohailey02@gmail.com>
2026-03-11 18:00:15 -0400

    refactor(movies): move  movie route handlers into movieController

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,6 @@
-const path = require("path");
+const moviesController = require("../controller/moviesController");
+
 const fs = require("fs");
-const crypto = require("crypto");
 const express = require("express");
 const router = express.Router();
 
@@ -8,65 +8,17 @@ const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-//DataPath
-const dataPath = path.join(__dirname, "../data/movies.json");
-
 //GET
-router.get("/", (req, res) => {
-  fs.readFile(dataPath, "utf-8", (err, data) => {
-    res.send(JSON.parse(data));
-  });
-});
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(`ID = ${id}`);
-  fs.readFile(dataPath, "utf-8", (err, data) => {
-    const movies = JSON.parse(data);
+router.get("/", moviesController.getMovies);
 
-    const target = movies.find((movie) => movie.id === id);
-    if (target) {
-      res.send(target);
-      return;
-    }
-    // Handle error
-    res.send("Error! Invalid id !!!");
-  });
-});
+router.get("/:id", moviesController.getMovieById);
 
 //POST
-router.post("/", (req, res) => {
-  const movieId = crypto.randomUUID();
-  const movieName = req.body.name;
-
-  // Read from DB
-  fs.readFile(dataPath, (err, data) => {
-    const movieList = JSON.parse(data);
-    // Add new movie into array movies
-    movieList.push({ id: movieId, name: movieName });
-
-    fs.writeFile(dataPath, JSON.stringify(movieList), () => {
-      res.send("Successfully added new movie!");
-    });
-  });
-});
+router.post("/", moviesController.createMovie);
 
 //PUT
-router.put("/:id", (req, res) => {
-  const targetId = req.params.id;
-  // Get newName from request body
-  const newName = req.body.name;
-
-  fs.readFile(dataPath, (err, data) => {
-    const movieList = JSON.parse(data);
-    const index = movieList.findIndex((movie) => movie.id === targetId);
-    //Modify the movie data
-    movieList[index] = { ...movieList[index], name: newName };
-    fs.writeFile(dataPath, JSON.stringify(movieList), () => {
-      res.send(`Movie with id ${targetId} has change its name to ${newName}`);
-    });
-  });
-});
+router.put("/:id", moviesController.updateMovie);
 
 // DELETE
 
@@ -82,20 +34,7 @@ router.delete("/1stMovie", (req, res) => {
   });
 });
 
-router.delete("/:id", (req, res) => {
-  const idRemove = req.params.id;
-  fs.readFile(dataPath, (err, data) => {
-    const moviesList = JSON.parse(data);
-    const index = moviesList.findIndex((element) => element.id === idRemove);
-    console.log(index);
-    // perform remove
-    const removeArr = moviesList.splice(index, 1);
-
-    fs.writeFile(dataPath, JSON.stringify(moviesList), () => {
-      res.send(`Movie ${removeArr[0].name} removed!`);
-    });
-  });
-});
+router.delete("/:id", moviesController.deleteMovie);
 
 // Must export to be use !!!
 module.exports = router;
