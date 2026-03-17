554529e4e4cc90d90025e53ceb83eb6c256ff6ac
Hailey <ngohailey02@gmail.com>
2026-03-17 14:51:06 -0400

    fix: fix CORS error

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
 const PORT = 3500;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 //Router Import
 const movies = require("./routes/movies");
 app.use("/movies", movies);
