cbdeb06dfdf5cf6d05750e1e0bdd7f08d7f64db3
Hailey <ngohailey02@gmail.com>
2026-03-20 18:41:30 -0400

    fix: add more properties to Movie schema and fix controller handler

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -1,13 +1,31 @@
 const Movie = require("../model/Movie");
 
 exports.getAllMovies = async (req, res) => {
-  const movie = await Movie.find();
-  res.json(movie);
+  const movies = await Movie.find();
+  const code = 200;
+
+  res.status(code).json(movies);
 };
 
 exports.createMovie = async (req, res) => {
-  await Movie.create({ title: req.body.title });
-  res.send(`Movie  added!`);
+  const newMovie = {
+    isFavorite: req.body.isFavorite,
+    title: req.body.title,
+    titleCustom: req.body.titleCustom,
+    personalRating: Number(req.body.personalRating),
+    note: req.body.note,
+  };
+  // Perform validation
+  if (newMovie.title === "") {
+    res.status(404).json({ message: "ERROR: Movie title CAN'T  EMPTY " });
+  }
+
+  if (newMovie.personalRating < 0 || newMovie.personalRating > 5) {
+    res.status(404).json({ message: "ERROR: Invalid rating  " });
+  }
+
+  const movie = await Movie.create(newMovie);
+  res.status(201).json(movie);
 };
 
 exports.updateMovie = async (req, res) => {
--- model/Movie.js
+++ model/Movie.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
+  isFavorite: { type: Boolean, default: false },
   title: {
     type: String,
     required: true,
   },
+  titleCustom: String,
+  personalRating: { type: Number, min: 0, max: 5 },
+  note: String,
+  dateCreated: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
